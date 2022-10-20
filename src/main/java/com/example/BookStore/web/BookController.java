28869df42a0342db14fe0ae93e11855f24dc09a2
olgatyynela <tapiotyynela@88-113-179-19.elisa-laajakaista.fi>
2022-10-20 22:47:36 +0300

    added delete button

--- src/main/java/com/example/BookStore/web/BookController.java
+++ src/main/java/com/example/BookStore/web/BookController.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.example.BookStore.domain.Book;
 import com.example.BookStore.domain.BookRepository;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @Controller
 public class BookController {
@@ -18,4 +22,25 @@ public class BookController {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
 	  }
-}
+	@GetMapping("/addbookpage")
+	public String addBookpage(Model model) {
+		model.addAttribute("book", new Book());
+	    return "addbook";
+	}
+	
+	@PostMapping("/addbook")
+	public String addBook(@ModelAttribute Book book, Model model) {
+		repository.save(book);
+		model.addAttribute("books", repository.findAll());
+	    return "booklist";
+	}
+	
+	@GetMapping("/deletebook/{id}")
+	public String deleteBook(@PathVariable("id") String bookId, Model model) {
+		long id = Integer.parseInt(bookId);
+		repository.deleteById(id);
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
+	}
+
+ }
