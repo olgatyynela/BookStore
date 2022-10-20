195e9fc53059fbebcd2599ebe6759b2d0a0741b1
olgatyynela <tapiotyynela@88-113-179-19.elisa-laajakaista.fi>
2022-10-20 23:44:17 +0300

    added edit page

--- src/main/java/com/example/BookStore/web/BookController.java
+++ src/main/java/com/example/BookStore/web/BookController.java
@@ -1,13 +1,17 @@
 package com.example.BookStore.web;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
 import com.example.BookStore.domain.Book;
 import com.example.BookStore.domain.BookRepository;
+
 import org.springframework.web.bind.annotation.PathVariable;
 
 
@@ -41,6 +45,23 @@ public class BookController {
 		repository.deleteById(id);
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
+	}
+	
+	@GetMapping("/editbookpage/{id}")
+	public String editBookpage(@PathVariable("id") String bookId, Model model) {
+		long id = Integer.parseInt(bookId);
+		model.addAttribute("booktoedit", repository.findById(id));
+		model.addAttribute("bookid", bookId);
+		return "editbook";
+	}
+	
+	@PostMapping("/editbook/{id}")
+	public String editBook(@PathVariable("id") String bookId, @ModelAttribute Book editedBook, Model model) {
+		long id = Integer.parseInt(bookId);
+		editedBook.setId(id);
+		repository.save(editedBook);
+		model.addAttribute("books", repository.findAll());
+	    return "booklist";
 	}
 
  }
